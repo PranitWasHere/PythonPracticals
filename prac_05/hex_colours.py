603d5ccf6ddb085d9cfb3c27202547b6de756f10
PranitWasHere <Pranit101@gmail.com>
2022-08-14 15:25:43 +0800

    add prac_05

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,23 +1,13 @@
 def main():
-    email_to_name = {}
-    email = input("Email: ")
-    while email != "":
-        name = get_name_from_email(email)
-        confirmation = input("Is your name {}? (Y/n) ".format(name))
-        if confirmation.upper() != "Y" and confirmation != "":
-            name = input("Name: ")
-        email_to_name[email] = name
-        email = input("Email: ")
+    COLORS = {"ALICEBLUE": "#f0f8ff", "BISTRE": "#3d2b1f"}
+    color = input("Color: ").upper()
+    while color != "":
+        if color in COLORS:
+            print(color, "is", COLORS[color])
+        else:
+            print("Invalid color.")
+        color = input("color: ").upper()
 
-    for email, name in email_to_name.items():
-        print("{} ({})".format(name, email))
-
-
-def get_name_from_email(email):
-    prefix = email.split('@')[0]
-    parts = prefix.split('.')
-    name = " ".join(parts).title()
-    return name
 
 
 main()
